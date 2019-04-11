85fbb816175a1f15defa0c711a06333b11d060be
justin collins <justin.scott.collins@gmail.com>
2019-04-11 11:51:42 -0600

    added relocate method and tests, added javadoc

--- src/main/java/com/tamr/geo/TamrGeoUtils.java
+++ src/main/java/com/tamr/geo/TamrGeoUtils.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.ParseException;
 
 import org.locationtech.spatial4j.context.SpatialContext;
-import org.locationtech.spatial4j.context.SpatialContextFactory;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
 import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
 import org.locationtech.spatial4j.exception.InvalidShapeException;
@@ -13,12 +12,14 @@ import org.locationtech.spatial4j.io.ShapeReader;
 import org.locationtech.spatial4j.io.ShapeWriter;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeFactory;
 import org.locationtech.spatial4j.shape.SpatialRelation;
-import org.locationtech.spatial4j.shape.jts.JtsGeometry;
+import org.locationtech.spatial4j.shape.ShapeFactory.LineStringBuilder;
 import org.locationtech.spatial4j.shape.jts.JtsShapeFactory;
 
 import com.vividsolutions.jts.algorithm.match.HausdorffSimilarityMeasure;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.util.AffineTransformation;
 
 public class TamrGeoUtils {
 
@@ -27,6 +28,9 @@ public class TamrGeoUtils {
 	private SpatialContext ctx = null;
 	JtsSpatialContextFactory scFactory = null;
 
+	/**
+	 * Create a TamrGeoUtils object.
+	 */
 	public TamrGeoUtils() {
 		scFactory = new JtsSpatialContextFactory();
 		scFactory.geo = true;
@@ -38,28 +42,83 @@ public class TamrGeoUtils {
 
 	}
 	
+	/**
+	 * Get the underlying {@link SpatialContext}.  This should not be needed by most clients but if you 
+	 * need to interact with the underlying libraries this is your access to it. 
+	 * For example, create a shape manually:
+	 * <pre>{@code
+	 *   ShapeFactory sf = gu.getSpatialContext().getShapeFactory();
+	 *   LineStringBuilder builder = sf.lineString();
+	 *   builder.pointXY(someX, someY);
+	 *   builder.pointXY(anotherX, anotherY);
+	 *   Shape lineBetweenPoints = builder.build();
+     * }</pre>
+     *
+	 * @return SpatialContext the underlying spatial context
+	 */
 	public SpatialContext getSpatialContext() {
 		return ctx;
 	}
 
+	/**
+	 * Create a {@link Shape} from a geoJson String.  This function does NOT support FeatureCollection, and none of the 
+	 * other methods in this class support FeatureCollection.  Attempting to read a FeatureCollection will throw 
+	 * ParseException. 
+	 * 
+	 * @param geoJsonString A valid geoJson String
+	 * @return Shape The shape described by the geoJson.
+	 * @throws InvalidShapeException
+	 * @throws IOException
+	 * @throws ParseException
+	 */
 	public Shape fromGeoJson(String geoJsonString) throws InvalidShapeException, IOException, ParseException {
 		return reader.read(geoJsonString);
 	}
 
+	/**
+	 * Output the given Shape as a geoJson String
+	 * 
+	 * @param shape The Shape to output geoJson for
+	 * @return A String representing this Shape as a geoJson String
+	 */
 	public String toGeoJson(Shape shape) {
 		return writer.toString(shape);
 	}
 
+	/**
+	 * Return a Point representing the centroid of the given Shape.  If the Shape is a Point, the returned Point will be 
+	 * identical to the Point passed in.  If the Shape passed in is a Line, the Point returned will be the midpoint of the 
+	 * Line, and will be on the line.  If the Shape passed in is a Polygon the Point returned will be the centroid of the 
+	 * Polygon, but <i>may not be contained inside the polygon</i> in the case of a Polygon with a 'donut hole' in the middle, 
+	 * or a 'U' shaped Polygon, etc.
+	 * 
+	 * @param geometry The Shape to calculate the centroid of.
+	 * @return a Point representing the centroid of this Shape.
+	 */
 	public Point getCentroid(Shape geometry) {
 		return geometry.getCenter();
 	}
 
+	/**
+	 * Calculate the distance between two Points, in Meters.
+	 * 
+	 * @param p1 Point 1
+	 * @param p2 Point 2
+	 * @return The distance between the two points in Meters
+	 */
 	public double calculateDistance(Point p1, Point p2) {
 		double radiansDistance = ctx.getDistCalc().distance(p1, p2);
 		double metersDistance = Math.toRadians(radiansDistance) * 6371000;
 		return metersDistance;
 	}
 
+	/**
+	 * Return true if the given Point is contained in the given Polygon.
+	 * 
+	 * @param polygon The Polygon
+	 * @param p1 The Point to test 
+	 * @return True if the Point is contained in the given Polygon, False otherwise
+	 */
 	public boolean polygonContainsPoint(Shape polygon, Point p1) {
 		SpatialRelation relate = polygon.relate(p1);
 		if (relate.intersects()) {
@@ -69,32 +128,98 @@ public class TamrGeoUtils {
 	}
 	
 
+	/**
+	 * Calculate the area of the give Shape in square meters.  By definition if the Shape passed in is a Point or Line this 
+	 * method will return 0.0.  Only Polygons have Area.  This calculation assumes a spherical earth, in fact the earth is 
+	 * ellipsoidal, which will cause this estimate to be accurate to approximately .3%.  More information 
+	 * <a href="https://gis.stackexchange.com/questions/25494/how-accurate-is-approximating-the-earth-as-a-sphere#25580">here</a>
+	 * 
+	 * @param geometry Shape to calculate area of
+	 * @return  The Area of the Shape in square meters
+	 */
 	public double calculateArea(Shape geometry) {
 		double squareRadians = geometry.getArea(ctx);
-		// this isn't correct, but it's a reasonable approximation and close enough for
-		// now.  Nominal accuracy of .3%
-
 		// To convert square degrees to meters, note that the radius of a sphere whose area is equal 
 		// to that of the earth's ellipsoidal surface (an authalic sphere) is 6371 km, giving 
 		// 111,194.9 meters per degree. 
 		return Math.toRadians(squareRadians) * 6371000 * 111194.9;
 	}
 	
-	public double getIntersectionArea(Shape s1, Shape s2) {
-		JtsShapeFactory shapeFactory = (JtsShapeFactory)scFactory.makeShapeFactory(ctx);
-		Geometry s1Geo = shapeFactory.getGeometryFrom(s1);
-		Geometry s2Geo = shapeFactory.getGeometryFrom(s2);
+	/**
+	 * Return a Shape representing the area of intersection between the two provided Shapes.
+	 * By definition the intersection of a Point within a Polygon is the Point, or a Line within 
+	 * a Polygon is the portion of the Line that is contained within the Polygon.
+	 * 
+	 * @param s1 Shape 1
+	 * @param s2 Shape 2
+	 * @return The Shape of overlap between the two provided Shapes, or an empty Shape if there is no overlap (does not return null in that case)
+	 */
+	public Shape getIntersection(Shape s1, Shape s2) {
+		Geometry s1Geo = getGeometryFrom(s1);
+		Geometry s2Geo = getGeometryFrom(s2);
 		Geometry intersection = s1Geo.intersection(s2Geo);
-		Shape resultShape = shapeFactory.makeShape(intersection);
+		return ((JtsShapeFactory)scFactory.makeShapeFactory(ctx)).makeShape(intersection);
+	}
+	
+	/**
+	 * Get the area of the intersection of the two provided Shapes in square Meters.  By definition the intersection of a Point 
+	 * with anything or a Line with anything will return 0.0, as Lines and Points have no area.
+	 * 
+	 * @param s1 Shape 1
+	 * @param s2 Shape 2
+	 * @return The area of overlap between the two shapes in square meters
+	 */
+	public double getIntersectionArea(Shape s1, Shape s2) {
+		Shape resultShape = getIntersection(s1, s2);
 		return calculateArea(resultShape);
 	}
 	
+	/**
+	 * Move the Shape src to the location of Shape dest.  More specifically, move the centroid of src to the locaiton of the 
+	 * centroid of dest.  Although the primary use case of this is to compare Shapes similarity regardless of relative location 
+	 * note that there is no guarantee that the relocated shape will have more overlap than pre-relocation.
+	 *  
+	 * @param src  The shape to relocate
+	 * @param dest The shape that provides the location to move src to.
+	 * @return a new Shape that is identical to src, but relocated so that its centroid is now located at the centroid of dest
+	 */
+	public Shape relocate(Shape src, Shape dest) {
+		Geometry srcGeo = getGeometryFrom(src);
+		Point srcCentroid = getCentroid(src);
+		Point destCentroid = getCentroid(dest);
+		double xOffset = destCentroid.getX() - srcCentroid.getX();
+		double yOffset = destCentroid.getY() - srcCentroid.getY();
+		AffineTransformation translationInstance = AffineTransformation.translationInstance(xOffset, yOffset);
+		Geometry transformedGeo = translationInstance.transform(srcGeo);
+		Shape resultShape = ((JtsShapeFactory)scFactory.makeShapeFactory(ctx)).makeShape(transformedGeo);
+		return resultShape;
+			
+	}
+	
+	/**
+	 * Measures the degree of similarity between two {@link Geometry}s
+	 * using the Hausdorff distance metric.
+	 * The measure is normalized to lie in the range [0, 1].
+	 * Higher measures indicate a greater degree of similarity.
+	 * <p>
+	 * The measure is computed by computing the Hausdorff distance
+	 * between the input geometries, and then normalizing
+	 * this by dividing it by the diagonal distance across 
+	 * the envelope of the combined geometries.
+	 * 
+	 * @param s1 Shape 1
+	 * @param s2 Shape 2
+	 * @return Hausdorff Similarity normalized to [0, 1]
+	 */
 	public double getHausdorffSimilarity(Shape s1, Shape s2) {
-		JtsShapeFactory shapeFactory = (JtsShapeFactory)scFactory.makeShapeFactory(ctx);
-		Geometry s1Geo = shapeFactory.getGeometryFrom(s1);
-		Geometry s2Geo = shapeFactory.getGeometryFrom(s2);
+		Geometry s1Geo = getGeometryFrom(s1);
+		Geometry s2Geo = getGeometryFrom(s2);
 		HausdorffSimilarityMeasure hausdorffSimilarityMeasure = new HausdorffSimilarityMeasure();
 		return hausdorffSimilarityMeasure.measure(s1Geo, s2Geo);
+	}
+	
+	protected Geometry getGeometryFrom(Shape s) {
+		return ((JtsShapeFactory)scFactory.makeShapeFactory(ctx)).getGeometryFrom(s);
 	}
 	
 
--- src/test/java/tamrgeocom/tamr/geo/GeoUtilsTest.java
+++ src/test/java/tamrgeocom/tamr/geo/GeoUtilsTest.java
@@ -19,13 +19,13 @@ import org.locationtech.spatial4j.shape.ShapeFactory.LineStringBuilder;
 import com.google.common.io.Resources;
 import com.tamr.geo.TamrGeoUtils;
 
-class GeoTestTest {
+class GeoUtilsTest {
 
 	private static final String GEOJSON_DIR = "testGeoJson";
 	
 	//Set acceptable deviance from known values of length at .03%, area at .3% (should be exponentially worse thanks to area calculations) 
 	private double ACCEPABLE_AREA_DEVIANCE = .003;
-	private double ACCEPABLE_LENGTH_DEVIANCE = .0003;
+	private double ACCEPTABLE_LENGTH_DEVIANCE = .0003;
 
 	private double ACTUAL_AREA_OF_PENTAGON = 97551.0;
 	private double ACTUAL_AREA_OF_LARGE_WIDE_AREA = 2075647987.0;
@@ -33,7 +33,9 @@ class GeoTestTest {
 	private double ACTUAL_AREA_OF_ONE_STORY_HOUSE = 73.42;
 	private double ACTUAL_AREA_OF_HIGH_LAT_HIGH_SCHOOL = 3877.59;
 	private double ACTUAL_DISTANCE_BTWN_HUMAN_ML_CENTROIDS = 3.815;
-	private double ACTUAL_DISTANCE_BTWN_IDENTICAL_BUILDING_CENTROIDS = 25.25;
+	private double ACTUAL_DISTANCE_BTWN_IDENTICAL_BUILDING_CENTROIDS = 25.61;
+	private double ACTUAL_HAUSDORFF_DISTANCE_BTWN_IDENTICAL_BUILDINGS = 0.62216;
+	private double ACTUAL_INTERSECTION_AREA = 700.31;
 
 
 
@@ -111,7 +113,7 @@ class GeoTestTest {
 
 		double centroidDifference = gu.calculateDistance(bldg1Centroid, bldg2Centroid);
 		double change = Math.abs((centroidDifference - ACTUAL_DISTANCE_BTWN_HUMAN_ML_CENTROIDS))/ACTUAL_DISTANCE_BTWN_HUMAN_ML_CENTROIDS;
-		//these two values are SO close together that even rounding error makes for higher percentage defiance.
+		//these two values are SO close together that even rounding error makes for higher percentage deviance.
 		//there is only 1.2 cm between these two values, but that's .32%, slightly above the nominal .3%
 		assertTrue(change < .0035);
 	}
@@ -129,11 +131,11 @@ class GeoTestTest {
 	@Test
 	void testLineCentroid() throws Exception {
 		TamrGeoUtils gu = new TamrGeoUtils();
-		String bldg1JsonString = readFile("identicalBuildingCentroid1.json");
-		Shape bldg1CentroidShape = gu.fromGeoJson(bldg1JsonString);
+		String bldg1CentoidJsonString = readFile("identicalBuildingCentroid1.json");
+		Shape bldg1CentroidShape = gu.fromGeoJson(bldg1CentoidJsonString);
 		Point bldg1Centroid = gu.getCentroid(bldg1CentroidShape);
-		String bldg2JsonString = readFile("identicalBuildingCentroid2.json");
-		Shape bldg2CentroidShape = gu.fromGeoJson(bldg2JsonString);
+		String bldg2CentoidJsonString = readFile("identicalBuildingCentroid2.json");
+		Shape bldg2CentroidShape = gu.fromGeoJson(bldg2CentoidJsonString);
 		Point bldg2Centroid = gu.getCentroid(bldg2CentroidShape);
 		
 		ShapeFactory sf = gu.getSpatialContext().getShapeFactory();
@@ -153,6 +155,18 @@ class GeoTestTest {
 		assertTrue(Math.abs(dist1 - dist2) < .0001);
 	}
 	
+	@Test
+	void testPolyCentroid() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1JsonString = readFile("identicalBuilding1.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		Point bldg1Centroid = gu.getCentroid(bldg1Shape);
+
+		String bldg1CentoidJsonString = readFile("identicalBuildingCentroid1.json");
+		Shape bldg1CentroidShape = gu.fromGeoJson(bldg1CentoidJsonString);
+		assertEquals(gu.getCentroid(bldg1Shape), bldg1CentroidShape);
+	}
+	
 	
 	@Test
 	void testDistanceBetweenIdenticalBuildingCentroids() throws Exception {
@@ -166,7 +180,7 @@ class GeoTestTest {
 
 		double centroidDifference = gu.calculateDistance(bldg1Centroid, bldg2Centroid);
 		double change = Math.abs((centroidDifference - ACTUAL_DISTANCE_BTWN_IDENTICAL_BUILDING_CENTROIDS))/ACTUAL_DISTANCE_BTWN_IDENTICAL_BUILDING_CENTROIDS;
-		assertTrue(change<ACCEPABLE_LENGTH_DEVIANCE);
+		assertTrue(change<ACCEPTABLE_LENGTH_DEVIANCE);
 	}
 
 	@Test
@@ -204,6 +218,14 @@ class GeoTestTest {
 	}
 	
 	@Test
+	void testReadInvalidFeatureCollectionJson() {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		assertThatThrownBy(() -> gu.fromGeoJson(readFile("InvalidFeatureCollection.json")))
+				.hasMessageContaining("Unknown type: FeatureCollection");
+
+	}
+	
+	@Test
 	void testWriteValidJson() throws Exception {
 		TamrGeoUtils gu = new TamrGeoUtils();
 		String geoString = readFile("uShapedMLGeneratedBuilding.json");
@@ -221,13 +243,86 @@ class GeoTestTest {
 		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
 		String bldg2JsonString = readFile("uShapedMLGeneratedBuilding.json");
 		Shape bldg2Shape = gu.fromGeoJson(bldg2JsonString);
+		
 		double intersectArea = gu.getIntersectionArea(bldg1Shape, bldg2Shape);
-		System.out.println("intersectionArea is "+intersectArea);
-		System.out.println("s1 area is "+gu.calculateArea(bldg1Shape));
-		System.out.println("s2 area is "+gu.calculateArea(bldg2Shape));
+		double change = Math.abs((intersectArea - ACTUAL_INTERSECTION_AREA))/ACTUAL_INTERSECTION_AREA;
+
+		//this will be higher than ACCEPABLE_AREA_DEVIANCE, as both the calculation of the overlap shape 
+		//and the calculation of area itself are very slightly different from geotools
+		assertTrue(change<(ACCEPABLE_AREA_DEVIANCE*2));
+		
 	
+		//Shape s = gu.getIntersection(bldg1Shape, bldg2Shape);
+		//System.out.println("intersection geojson is "+gu.toGeoJson(s));
 	}
 	
+	@Test
+	void testIntersectionAreaWithPoint() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1JsonString = readFile("identicalBuilding1.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		String bldg1CentroidJsonString = readFile("identicalBuildingCentroid1.json");
+		Shape bldg1CentroidShape = gu.fromGeoJson(bldg1CentroidJsonString);
+		
+		Shape intersectionShape = gu.getIntersection(bldg1Shape, bldg1CentroidShape);
+		assertTrue(gu.calculateDistance(gu.getCentroid(intersectionShape), gu.getCentroid(bldg1CentroidShape)) == 0.0);
+		
+		double intersectArea = gu.getIntersectionArea(bldg1Shape, bldg1CentroidShape);
+		//Points have no area, so the intersection area is 0.0
+		assertTrue(intersectArea == 0.0);
+	}
+	
+	@Test
+	void testEmptyIntersectionArea() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1JsonString = readFile("identicalBuilding1.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		String bldg2JsonString = readFile("identicalBuilding2.json");
+		Shape bldg2Shape = gu.fromGeoJson(bldg2JsonString);
+		
+		//there is no overlap here
+		Shape intersectionShape = gu.getIntersection(bldg1Shape, bldg2Shape);
+		System.out.println("empty intersection geojson is "+gu.toGeoJson(intersectionShape));
+		System.out.println("empty intersection hasArea "+intersectionShape.hasArea());
+
+		assertTrue(gu.calculateArea(intersectionShape) == 0.0);
+		
+		double intersectArea = gu.getIntersectionArea(bldg1Shape, bldg2Shape);
+		//Points have no area, so the intersection area is 0.0
+		assertTrue(intersectArea == 0.0);
+	}
+	
+	@Test
+	void testIntersectionAreaWithLine() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1CentoidJsonString = readFile("identicalBuildingCentroid1.json");
+		Shape bldg1CentroidShape = gu.fromGeoJson(bldg1CentoidJsonString);
+		Point bldg1Centroid = gu.getCentroid(bldg1CentroidShape);
+		String bldg2CentoidJsonString = readFile("identicalBuildingCentroid2.json");
+		Shape bldg2CentroidShape = gu.fromGeoJson(bldg2CentoidJsonString);
+		Point bldg2Centroid = gu.getCentroid(bldg2CentroidShape);
+		
+		ShapeFactory sf = gu.getSpatialContext().getShapeFactory();
+		
+		LineStringBuilder builder = sf.lineString();
+		builder.pointXY(bldg1Centroid.getX(), bldg1Centroid.getY());
+		builder.pointXY(bldg2Centroid.getX(), bldg2Centroid.getY());
+		Shape lineBetweenCentroids = builder.build();
+		
+		String bldg1JsonString = readFile("identicalBuilding1.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		
+		
+		//Shape intersectionShape = gu.getIntersection(bldg1Shape, lineBetweenCentroids);
+		double intersectArea = gu.getIntersectionArea(bldg1Shape, lineBetweenCentroids);
+		//Lines have no area, so the intersection area is 0.0
+		assertTrue(intersectArea == 0.0);
+	}
+	
+	/*
+	 * Check the hausdorff similarity between two versions of the same building, one created by ML, one by a human. 
+	 * Should be very high, compare to known value established by geotools.
+	 */
 	@Test
 	void testHausdorffSimilarityMLvsHumanBuilding() throws Exception {
 		TamrGeoUtils gu = new TamrGeoUtils();
@@ -239,20 +334,56 @@ class GeoTestTest {
 		assertTrue(hausdorffSimilarity < .94 && hausdorffSimilarity >.92);
 	}
 	
+	/*
+	 * Check the hausdorff similarity between two buildings that are nearby and almost identical shapes. 
+	 * Should be much lower than ML vs Human polys above, compare to known value established by geotools.
+	 */
 	@Test
 	void testHausdorffSimilarityIdenticalButDifferentBuildings() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1JsonString = readFile("identicalBuilding1.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		String bldg2JsonString = readFile("identicalBuilding2.json");
+		Shape bldg2Shape = gu.fromGeoJson(bldg2JsonString);
+		double hausdorffSimilarity= gu.getHausdorffSimilarity(bldg1Shape, bldg2Shape);
+		double change = Math.abs((hausdorffSimilarity - ACTUAL_HAUSDORFF_DISTANCE_BTWN_IDENTICAL_BUILDINGS))/ACTUAL_HAUSDORFF_DISTANCE_BTWN_IDENTICAL_BUILDINGS;
+		assertTrue(change < ACCEPTABLE_LENGTH_DEVIANCE);
+	}
+	
+	/*
+	 * Relocation of point to point.  No calculations done here, just moving the origin to the dest
+	 */
+	@Test
+	void testRelocateIdenticalButDifferentBuildings() throws Exception {
 		TamrGeoUtils gu = new TamrGeoUtils();
 		String bldg1JsonString = readFile("identicalBuildingCentroid1.json");
 		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
 		String bldg2JsonString = readFile("identicalBuildingCentroid2.json");
 		Shape bldg2Shape = gu.fromGeoJson(bldg2JsonString);
-		double hausdorffSimilarity= gu.getHausdorffSimilarity(bldg1Shape, bldg2Shape);
-		assertTrue(hausdorffSimilarity == 0.0);
+		Shape relocatedShape = gu.relocate(bldg1Shape, bldg2Shape);
+
+		//the only case where these will be TRUELY identical is for points, no rounding error there
+		assertTrue(gu.getCentroid(relocatedShape).equals(gu.getCentroid(bldg2Shape)));
+	}
+	
+	/*
+	 * Relocation of poly to poly.  Verify that the resulting centroid distance is within acceptable deviance
+	 */
+	@Test
+	void testRelocateMLVsHumanUshapedBldg() throws Exception {
+		TamrGeoUtils gu = new TamrGeoUtils();
+		String bldg1JsonString = readFile("uShapedHumanGeneratedBuilding.json");
+		Shape bldg1Shape = gu.fromGeoJson(bldg1JsonString);
+		String bldg2JsonString = readFile("uShapedMLGeneratedBuilding.json");
+		Shape bldg2Shape = gu.fromGeoJson(bldg2JsonString);
+		Shape relocatedShape = gu.relocate(bldg1Shape, bldg2Shape);
+
+		assertTrue(gu.calculateDistance(gu.getCentroid(relocatedShape), gu.getCentroid(bldg2Shape)) < ACCEPTABLE_LENGTH_DEVIANCE);
 	}
 
 	static String readFile(final String fileName) throws IOException {
 		final String path = String.format("%s/%s", GEOJSON_DIR, fileName);
-		final URL responsesUrl = GeoTestTest.class.getClassLoader().getResource(path);
+		final URL responsesUrl = GeoUtilsTest.class.getClassLoader().getResource(path);
 		return Resources.toString(responsesUrl, UTF_8);
 	}
 
